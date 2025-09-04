832bd54ef66b0526e1d408ac68630945ec9b55cb
Unitedimmi <unitedimmi.12345@gmail.com>
2025-09-05 00:03:40 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URI = "mongodb+srv://ayush2211207:koLO50CvMTHHJ1Wg@cluster0.d7fkhvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://admin:admin@cluster0.ifswddf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.immigration
